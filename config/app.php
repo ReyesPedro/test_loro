f3aab1dccbb1f4594c9496769399a4ae61ed2a20
Pedro Reyes <pedro.reyes63@yahoo.es>
2019-10-09 22:12:49 -0500

    Se agrega el framework laravel 5.8 al proyecto Se configura el paquete jenssegers/laravel-mongodb Se configura el archivo database.php

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Nuevos Service Providers
+         */
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
+
     ],
 
     /*
